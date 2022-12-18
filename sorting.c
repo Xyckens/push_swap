1b78bec94d9adcc5242d0d2e6196d9d44ec74152
Francisco Vieira <fvieira@c1r7s2.42porto.com>
2022-12-18 20:31:00 +0000

    no advancements

--- sorting.c
+++ sorting.c
@@ -1,40 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fvieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/16 20:19:03 by fvieira           #+#    #+#             */
-/*   Updated: 2022/12/16 20:19:10 by fvieira          ###   ########.fr       */
+/*   Created: 2022/12/18 15:49:55 by fvieira           #+#    #+#             */
+/*   Updated: 2022/12/18 15:49:57 by fvieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 
-int	checker(int *stack, int len)
-{
-	int count;
-	int	count2;
-	int	flag;
-
-	count = 0;
-	flag = 0;
-	while (count < len)
-	{
-		count2 = count + 1;
-		while (count2 < len)
-		{
-			if (stack[count] == stack[count2] || stack[count] > INT_MAX
-				|| stack[count] < INT_MIN)
-			{
-				flag = 1;
-				ft_printf("Error\n");
-				break ;
-			}
-			count2++;
-		}
-		count++;
-	}
-	return (flag);
-}
